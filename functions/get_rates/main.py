f2b7739d0ee977198201bfe1230493c1015ddc99
Paul Harrison <paulharrison2@gmail.com>
2017-10-01 12:45:32 +0100

    Add base and uuid.

--- functions/get_rates/main.py
+++ functions/get_rates/main.py
@@ -1,22 +1,24 @@
 import json
 import requests
 from decimal import Decimal
+from uuid import uuid4
 
 print('Loading function')
 
 
 def reformat_json(response):
     rates = response.pop("rates")
-    response.pop("base")
     response.update(rates)
     response_updated = {}
     for key, val in response.items():
-        if key == "date":
+        if key == "date" or key == "base":
             val_type = "S"
         else:
             val_type = "N"
             val = Decimal(str(val))
         response_updated[key] = {val_type: val}
+
+    response_updated["uuid"] = {"S": uuid4()}
 
     return response_updated
 
