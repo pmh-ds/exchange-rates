f354825e2512fd71fcba90f42e8b098f51a10db5
Paul Harrison <paulharrison2@gmail.com>
2017-10-01 12:20:21 +0100

    Flatten JSON response.

--- functions/get_rates/main.py
+++ functions/get_rates/main.py
@@ -4,7 +4,17 @@ import requests
 print('Loading function')
 
 
+def reformat_json(response):
+    rates = response.pop("rates")
+    response.update(rates)
+
+    return response
+
+
 def handle(event, context):
     response = requests.get("http://api.fixer.io/latest?base=GBP")
     print(response.status_code)
-    return response.json()
+
+    response_json = reformat_json(response.json())
+
+    return response_json
