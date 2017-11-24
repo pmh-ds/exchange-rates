05c4566a29d89ec726844b9f01396f9525f6299f
Paul Harrison <paulharrison2@gmail.com>
2017-11-24 20:48:46 +0000

    Add deploy and clean hooks.

---
+++ functions/get_rates/clean_up.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+rm -r boto*
+rm -r certifi*
+rm -r chardet*
+rm -r dateutil*
+rm -r docutils*
+rm -r idna*
+rm -r jmespath*
+rm -r python_dateutil*
+rm -r requests*
+rm -r s3transfer*
+rm -r six*
+rm -r urllib3*
+rm -r nose*
